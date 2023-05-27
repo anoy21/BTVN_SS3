fda6048e332860e4347a4fc6a710f602b7d8d64a
anoy21 <thienhung.kp2@gmail.com>
2023-05-28 00:12:32 +0700

    latest

--- app.js
+++ app.js
@@ -26,61 +26,14 @@ for (var i = 0; i <= arrayLength; i++) {
 console.log(even)
 
 
-// var choice;
-// YesNo = window.prompt("have you eaten? Yes or No")
-// YesNo2 = window.prompt("Do you want cake? Yes or No")
-// YesNo3 = window.prompt("Do you want a soda aswell? Yes or No")
-// YesNo4 = window.prompt("Do you want a meal? Yes or No")
-// YesNo5 = window.prompt("Oh? Well take care ")
-// answer = []
-
-// if (YesNo = "Yes") {
-//     answer.push("Yes")
-//     YesNo2 = window.prompt("Do you want cake? Yes or No")
-//     if (YesNo2 = "Yes") {
-//         answer.push("Yes")
-    
-//         YesNo3 = window.prompt("Do you want a soda aswell? Yes or No")
-//         if (YesNo3 = "Yes") {
-//             answer.push("Yes")
-//         } 
-//         else {
-//             {answer.push("No")
-//             window.alert("Oh? Well take care ")
-//         }
-//     }
-
-//     }
-//     else {answer.push("No")
-//     window.alert("Oh? Well take care ")
-
-// }
-// }
-// else {
-//     answer.push("No")
-//     YesNo4 = window.prompt("Do you want a meal? Yes or No")
-//     if (YesNo4 = "Yes") {
-//         answer.push("Yes")
-//     }
-//     else {
-//         answer.push["No"]
-//         YesNo5 = window.alert("Oh? Well take care ")
-//     }
-// }
-
-
-
-// console.log(answer)
-
 
 //bai 3 
 
-myAns = []
 
-var arr = [];                               // define our array
-
-for (var i = 0; i < 6; i++) {              // loop 10 times
-  arr.push(prompt('Enter grade ' + (i+1))); // push the value into the array
+var myQues = ["Whats your name", "How old are you?", "What's your favorite food?", "What is your hobby?", "When's your birthday"]
+var myAns = [];                              
+for (var i = 0; i < 6; i++) { 
+    myAns.push(prompt(myQues[i]));
 }
 
-alert('Full array: ' + arr.join(', ')); 
+alert('Answers: ' + myAns.join(' ; ')); 
